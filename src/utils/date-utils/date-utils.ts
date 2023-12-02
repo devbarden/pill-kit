684c113f21595b54004cbc1350dc9b58656390e3
devbarden <denis.bar1997@gmail.com>
2023-12-03 02:48:09 +0300

    fix: percentage calculating

--- src/utils/date-utils/date-utils.ts
+++ src/utils/date-utils/date-utils.ts
@@ -1,19 +1,9 @@
-export const getDaysBetweenDates = (
-	firstDate: number,
-	secondDate: number,
-): number =>
-	Math.round(Math.abs((firstDate - secondDate) / (24 * 60 * 60 * 1000)))
-
 export const getDivideValueByDates = (
 	startDate: number,
 	endDate: number,
 ): number => {
-	const allDays = getDaysBetweenDates(endDate, Date.now())
-	const leftDays = getDaysBetweenDates(endDate, startDate)
-
-	const diff = allDays / leftDays
-	const roundedValue = (1 - diff).toFixed(2)
-	const result = parseFloat(roundedValue)
+	const value = (Date.now() - startDate) / (endDate - startDate)
+	const result = parseFloat(value.toFixed(2))
 
 	return result
 }
