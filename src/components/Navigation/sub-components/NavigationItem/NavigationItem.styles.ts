f009ab717f7bc137074dfc679ca930ed8d78da52
devbarden <denis.bar1997@gmail.com>
2024-01-16 18:19:51 +0300

    fix: android safe area

--- src/components/Navigation/sub-components/NavigationItem/NavigationItem.styles.ts
+++ src/components/Navigation/sub-components/NavigationItem/NavigationItem.styles.ts
@@ -1,10 +1,12 @@
 import { StyleSheet } from 'react-native'
 
+import { IS_ANDROID } from '@app/constants'
+
 export const styles = StyleSheet.create({
 	wrapper: {
 		flex: 1,
 		paddingTop: 16,
-		paddingBottom: 8,
+		paddingBottom: IS_ANDROID ? 16 : 8,
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
--- src/components/SafeArea/SafeArea.styles.ts
+++ src/components/SafeArea/SafeArea.styles.ts
@@ -1,10 +1,12 @@
-import { StyleSheet } from 'react-native'
+import { StatusBar, StyleSheet } from 'react-native'
 
 import { EnumColor } from '@app/enums'
+import { IS_ANDROID } from '@app/constants'
 
 export const styles = StyleSheet.create({
 	wrapper: {
 		flex: 1,
 		backgroundColor: EnumColor.white,
+		paddingTop: IS_ANDROID ? StatusBar.currentHeight : 0,
 	},
 })
