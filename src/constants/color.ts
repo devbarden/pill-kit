77b37a69c49eed5f037bad6b68a6a6f5554e7f9e
devbarden <denis.bar1997@gmail.com>
2024-01-17 17:45:07 +0300

    fix: medicines colors

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -3,13 +3,13 @@ import { EnumMedicineColor, EnumMedicineType } from '@app/enums'
 export const MEDICINE_TYPE_COLORS: Record<EnumMedicineType, EnumMedicineColor> =
 	{
 		[EnumMedicineType.pill]: EnumMedicineColor.red,
-		[EnumMedicineType.liquid]: EnumMedicineColor.black,
-		[EnumMedicineType.injection]: EnumMedicineColor.champagne,
-		[EnumMedicineType.cream]: EnumMedicineColor.lightBlue,
-		[EnumMedicineType.drops]: EnumMedicineColor.blue,
-		[EnumMedicineType.candles]: EnumMedicineColor.darkBlue,
-		[EnumMedicineType.pencil]: EnumMedicineColor.green,
+		[EnumMedicineType.liquid]: EnumMedicineColor.blue,
+		[EnumMedicineType.injection]: EnumMedicineColor.darkGreen,
+		[EnumMedicineType.cream]: EnumMedicineColor.darkRed,
+		[EnumMedicineType.drops]: EnumMedicineColor.lightBlue,
+		[EnumMedicineType.candles]: EnumMedicineColor.darkOrange,
+		[EnumMedicineType.pencil]: EnumMedicineColor.darkPurple,
 		[EnumMedicineType.powder]: EnumMedicineColor.orange,
-		[EnumMedicineType.spray]: EnumMedicineColor.darkRed,
-		[EnumMedicineType.bandage]: EnumMedicineColor.purple,
+		[EnumMedicineType.spray]: EnumMedicineColor.darkBlue,
+		[EnumMedicineType.bandage]: EnumMedicineColor.lightPurple,
 	}
