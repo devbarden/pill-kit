d6f5177df8c350010a006052fa9ab722635ccc81
devbarden <denis.bar1997@gmail.com>
2024-02-03 20:27:53 +0300

    fix: configuration locale

--- src/constants/configuration.ts
+++ src/constants/configuration.ts
@@ -3,18 +3,9 @@ import * as Localization from 'expo-localization'
 import { TypeConfiguration } from '@app/types'
 
 import { PHONE_THEME } from './theme'
-import { ARABIC_NUMBER_CODE } from './language'
-
-const getLocale = () => {
-	if (Localization.locale.includes('ar')) {
-		return ARABIC_NUMBER_CODE
-	}
-
-	return Localization.locale
-}
 
 export const INITIAL_APP_CONFIGURATION: TypeConfiguration = {
 	theme: PHONE_THEME,
 	isUserAcceptAppDocs: false,
-	language: getLocale(),
+	language: Localization.locale,
 }
